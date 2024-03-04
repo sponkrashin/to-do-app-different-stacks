9067ba35f9f8ba731924c5066616e6d99c2cc138
Sergei Ponkrashin <ponkrashin.sv@gmail.com>
2024-03-05 01:30:25 +0300

    Fix server CORS issue

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,12 @@
 import cookieParserMiddleware from 'cookie-parser';
+import cors from 'cors';
 import express from 'express';
 import loggerMiddleware from 'morgan';
 import todoRouter from './routes/todosRoute';
 
 const app = express();
 
+app.use(cors());
 app.use(loggerMiddleware('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
