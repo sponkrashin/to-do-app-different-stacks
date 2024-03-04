6cbfa0ab0313c528f814ab6f258387775cf831e4
Sergei Ponkrashin <ponkrashin.sv@gmail.com>
2024-03-05 01:37:36 +0300

    Fix issues with closed database connection on server

--- server/src/repositories/databaseClient.ts
+++ server/src/repositories/databaseClient.ts
@@ -3,44 +3,27 @@ import { Db, MongoClient } from 'mongodb';
 let CONNECTION_STRING = '';
 let DATABASE_NAME = '';
 
-export class DatabaseClient {
-  constructor(
-    private client: MongoClient,
-    public database: Db,
-  ) {}
-
-  async execute<T>(handler: (database: Db) => Promise<T>): Promise<T> {
-    try {
-      const result = await handler(this.database);
-      return result;
-    } finally {
-      this.client.close();
-    }
-  }
-
-  close(): Promise<void> {
-    return this.client.close();
-  }
-}
-
 export function init(connectionString: string, databaseName: string): void {
   CONNECTION_STRING = connectionString;
   DATABASE_NAME = databaseName;
 }
 
-export function connect(): DatabaseClient {
-  if (!CONNECTION_STRING || !DATABASE_NAME) {
-    throw new Error('Connection string and database name should be set.');
-  }
+export class DatabaseClient {
+  async execute<T>(handler: (database: Db) => Promise<T>): Promise<T> {
+    if (!CONNECTION_STRING || !DATABASE_NAME) {
+      throw new Error('Connection string and database name should be set.');
+    }
 
-  const client = new MongoClient(CONNECTION_STRING);
+    const client = new MongoClient(CONNECTION_STRING);
 
-  try {
-    const database = client.db(DATABASE_NAME);
-    return new DatabaseClient(client, database);
-  } catch {
-    throw new Error('An error occurred while connecting to the database.');
-  } finally {
-    client.close();
+    try {
+      const database = client.db(DATABASE_NAME);
+      const result = await handler(database);
+      return result;
+    } catch {
+      throw new Error('An error occurred while connecting to the database.');
+    } finally {
+      client.close();
+    }
   }
 }
